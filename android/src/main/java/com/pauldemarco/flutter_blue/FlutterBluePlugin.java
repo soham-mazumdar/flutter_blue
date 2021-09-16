dbdb4381b24cd746a0c183cb53d55e45e20c2b09
Soham Mazumdar <mazumder.soham34@gmail.com>
2021-09-17 04:55:43 +0530

    Update FlutterBluePlugin.java

--- android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
@@ -536,7 +536,8 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
                     characteristic = locateCharacteristic(gattServer, request.getServiceUuid(), request.getSecondaryServiceUuid(), request.getCharacteristicUuid());
                     cccDescriptor = characteristic.getDescriptor(CCCD_ID);
                     if(cccDescriptor == null) {
-                        throw new Exception("could not locate CCCD descriptor for characteristic: " +characteristic.getUuid().toString());
+                                                log(LogLevel.INFO, "could not locate CCCD descriptor for characteristic: " +characteristic.getUuid().toString());
+//                         throw new Exception("could not locate CCCD descriptor for characteristic: " +characteristic.getUuid().toString());
                     }
                 } catch(Exception e) {
                     result.error("set_notification_error", e.getMessage(), null);
